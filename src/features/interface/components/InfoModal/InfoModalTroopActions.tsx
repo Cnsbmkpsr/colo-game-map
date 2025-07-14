46c42d37e28d7467eec413c5f5abe5a937f95f9b
ItsTheSky <livegogo.16334@gmail.com>
2025-07-14 03:48:14 +0200

    feat: improve InfoModalTroopActions to handle troop ownership and selected faction checks

--- src/features/interface/components/InfoModal/InfoModalTroopActions.tsx
+++ src/features/interface/components/InfoModal/InfoModalTroopActions.tsx
@@ -1,24 +1,25 @@
-import {VStack} from "@chakra-ui/react";
-import {factionStore} from "../../../game/factionStore";
-import {troopStore} from "../../../game/troopStore";
+import { VStack } from "@chakra-ui/react";
+import { factionStore } from "../../../game/factionStore";
+import { troopStore } from "../../../game/troopStore";
 import InfoModalBuyTroopsActions from "./InfoModalBuyTroopsActions";
 import InfoModalOwnedTroopAction from "./InfoModalOwnedTroopAction";
-import {observer} from "mobx-react";
-import {Faction} from "../../../game/types";
+import { observer } from "mobx-react";
+import { Faction } from "../../../game/types";
 
 const InfoModalTroopActions = () => {
-    const selectedFaction = factionStore.getSelectedFaction as Faction;
+  const selectedFaction = factionStore.getSelectedFaction as Faction;
 
-    return (
-        <VStack spacing={1} w="full">
-            {!troopStore.hasTroopOnSelectedCell && (
-                <InfoModalBuyTroopsActions factionColor={selectedFaction.color}/>
-            )}
-            {troopStore.hasTroopOnSelectedCell && (
-                <InfoModalOwnedTroopAction factionColor={selectedFaction.color}/>
-            )}
-        </VStack>
-    );
+  return (
+    <VStack spacing={1} w="full">
+      {!troopStore.hasTroopOnSelectedCell && selectedFaction && (
+        <InfoModalBuyTroopsActions factionColor={selectedFaction.color} />
+      )}
+      {selectedFaction && troopStore.hasTroopOnSelectedCell &&
+        troopStore.isTroopOwnedBySelectedFaction && (
+          <InfoModalOwnedTroopAction factionColor={selectedFaction.color} />
+        )}
+    </VStack>
+  );
 };
 
 export default observer(InfoModalTroopActions);
